8d2c3dc02ed5ec84f59d8570a97023aeece48222
agugoat <132964291+agugoat@users.noreply.github.com>
2024-03-04 12:35:10 -0500

    Add files via upload

--- HDatabase/src/bag/HDbase.java
+++ HDatabase/src/bag/HDbase.java
@@ -9,30 +9,28 @@ import java.util.Set;
 
 public class HDbase<T> extends AbstractCollection<T> {
 
-	Hashtable<T, Integer> heavyBagTable;
+	Hashtable<T, Integer> heavyTable;
 	// just created a hashtable for the bag
 
-	
 	public HDbase() {
-		heavyBagTable = new Hashtable<T, Integer>();
+		heavyTable = new Hashtable<T, Integer>();
 		// initializing a new hashtable
 	}
 
 	// method will add to the "bag"
 	@Override
 	public boolean add(T o) {
-		if (heavyBagTable.containsKey(o)) {
+		if (heavyTable.containsKey(o)) {
 			// Element already exists, increment its count
-			heavyBagTable.put(o, heavyBagTable.get(o) + 1);
+			heavyTable.put(o, heavyTable.get(o) + 1);
 		} else {
 			// Element not in the bag, add it with a count of 1
-			heavyBagTable.put(o, 1);
+			heavyTable.put(o, 1);
 		}
 		return true;
 		// always returns true
 	}
 
-	
 	public boolean addMany(T o, int count) {
 		if (count < 1) {
 			throw new IllegalArgumentException("Your less than one");
@@ -45,11 +43,11 @@ public class HDbase<T> extends AbstractCollection<T> {
 		}
 		// if the count is bigger than 1000000000
 
-		if (heavyBagTable.containsKey(o)) {
-			heavyBagTable.put(o, heavyBagTable.get(o) + count);
+		if (heavyTable.containsKey(o)) {
+			heavyTable.put(o, heavyTable.get(o) + count);
 			// if the key already exists add on to the existing element
 		} else {
-			heavyBagTable.put(o, count);
+			heavyTable.put(o, count);
 			// put the the element plus the count
 		}
 		return true;
@@ -60,14 +58,13 @@ public class HDbase<T> extends AbstractCollection<T> {
 	// Simple tooString method
 	public String toString() {
 		String answer = "";
-		for (T elements : heavyBagTable.keySet()) {
+		for (T elements : heavyTable.keySet()) {
 			answer += elements.toString();
 		}
 		return answer;
 	}
 	// Probably a bit basic but it works
 
-	
 	@Override
 	public boolean equals(Object o) { // works
 		if (this == o) {
@@ -76,18 +73,18 @@ public class HDbase<T> extends AbstractCollection<T> {
 		}
 		if (!(o instanceof HDbase)) {
 			return false;
-		
+
 			// if o is not a instance of heavybag return false
 		}
 
 		HDbase<?> other = (HDbase<?>) o;
 		// making a hashtable object
 		// Check if the Hashtables have the same size
-		if (this.heavyBagTable.size() != other.heavyBagTable.size()) {
+		if (this.heavyTable.size() != other.heavyTable.size()) {
 			return false;
 			// Check if the heavybags have the same size
 		}
-		for (T element : heavyBagTable.keySet()) {
+		for (T element : heavyTable.keySet()) {
 			if (other.contains(element) != this.contains(element)
 					|| other.getCount(element) != this.getCount(element)) {
 				return false;
@@ -100,9 +97,6 @@ public class HDbase<T> extends AbstractCollection<T> {
 		// alwats returning true
 	}
 
-	
-	
-	
 	@Override
 	// hashcode method
 	public int hashCode() {
@@ -110,12 +104,12 @@ public class HDbase<T> extends AbstractCollection<T> {
 		// prime number to multiply result with
 		int results = 1;
 		// results variable
-		for (T elements : heavyBagTable.keySet()) {
+		for (T elements : heavyTable.keySet()) {
 			if (elements == null) {
 				return 0;
 				// if elements = null return 0
 			} else {
-				int hash = (elements.hashCode() + heavyBagTable.get(elements));
+				int hash = (elements.hashCode() + heavyTable.get(elements));
 				// getting hashcode
 				int trueHash = hash * prime;
 				// caluating the true hash
@@ -130,14 +124,11 @@ public class HDbase<T> extends AbstractCollection<T> {
 		// return the results
 	}
 
-
-	
-	
 	// iterator method
 	@Override
 	public Iterator<T> iterator() {
 		return new Iterator<T>() {
-			private Iterator<T> keyIterator = heavyBagTable.keySet().iterator();
+			private Iterator<T> keyIterator = heavyTable.keySet().iterator();
 			// making a new keyIterator
 			private T currentKey = null;
 			// currentKey
@@ -157,7 +148,7 @@ public class HDbase<T> extends AbstractCollection<T> {
 			public T next() {
 				if (currentCount == 0) {
 					currentKey = keyIterator.next();
-					currentCount = heavyBagTable.get(currentKey);
+					currentCount = heavyTable.get(currentKey);
 					// Move to the next key and update the current count
 				}
 				currentCount--;
@@ -168,10 +159,10 @@ public class HDbase<T> extends AbstractCollection<T> {
 			@Override
 			public void remove() {
 				if (contains(currentKey)) {
-					int countInBag = heavyBagTable.get(currentKey);
+					int countInBag = heavyTable.get(currentKey);
 					if (countInBag > 1) {
 						// if count in the bag is bigger than 1
-						heavyBagTable.put(currentKey, countInBag - 1);
+						heavyTable.put(currentKey, countInBag - 1);
 						// removing count of one of the elements
 					} else {
 						keyIterator.remove();
@@ -182,12 +173,11 @@ public class HDbase<T> extends AbstractCollection<T> {
 		};
 	};
 
-	
 	public Set<T> uniqueElements() {
 		HashSet<T> uniqueElements = new HashSet<>();
 		// makes a new hashset
 
-		for (T uniElement : heavyBagTable.keySet()) {
+		for (T uniElement : heavyTable.keySet()) {
 			uniqueElements.add(uniElement);
 			// loop through the set and add one of each element
 			// sets to not allow duplicates
@@ -197,15 +187,12 @@ public class HDbase<T> extends AbstractCollection<T> {
 
 	}
 
-
-	
-	
 	public int getCount(Object o) {
 		if (o == null) {
 			return 0;
 			// null check
 		}
-		Integer count = heavyBagTable.get(o);
+		Integer count = heavyTable.get(o);
 		// getting count
 		if (count == null) {
 			return 0;
@@ -215,17 +202,14 @@ public class HDbase<T> extends AbstractCollection<T> {
 		// just return the count
 	}
 
-
-	
-	
 	// choosing a random element
 	public T choose(Random r) {
 		int totalSize = size();
 		// size of table
 		int randomIndex = r.nextInt(totalSize) + 1; // Generate a random number
 		// randomly generate number
-		for (T element : heavyBagTable.keySet()) {
-			int count = heavyBagTable.get(element);
+		for (T element : heavyTable.keySet()) {
+			int count = heavyTable.get(element);
 			// getting count
 
 			randomIndex = randomIndex - count;
@@ -239,12 +223,10 @@ public class HDbase<T> extends AbstractCollection<T> {
 
 	}
 
-	
-	
 	@Override
 	// contains method
 	public boolean contains(Object o) {
-		for (T element : heavyBagTable.keySet()) {
+		for (T element : heavyTable.keySet()) {
 			// looping through the heavy bag
 			if (element.equals(o)) {
 				// if elment equals the object
@@ -257,20 +239,17 @@ public class HDbase<T> extends AbstractCollection<T> {
 		// returns false
 	}
 
-	
-	
-	
 	@Override
 	public boolean remove(Object o) { // may not work , but it works
-		if (heavyBagTable.containsKey(o)) {
-			int count = heavyBagTable.get(o);
+		if (heavyTable.containsKey(o)) {
+			int count = heavyTable.get(o);
 			if (count > 1) {
-				heavyBagTable.put((T) o, count - 1);
+				heavyTable.put((T) o, count - 1);
 				// unchecked cast
 				// putting the element and the count minus 1
 
 			} else {
-				heavyBagTable.remove(o);
+				heavyTable.remove(o);
 				// remove 1 if the count is just 1
 			}
 
@@ -279,13 +258,10 @@ public class HDbase<T> extends AbstractCollection<T> {
 		return true;
 	}
 
-	
-	
-	
 	@Override
 	public int size() {
 		int totalSize = 0;
-		for (Integer size : heavyBagTable.values()) {
+		for (Integer size : heavyTable.values()) {
 			totalSize += size;
 			// just gets the total number of object instances
 		}
